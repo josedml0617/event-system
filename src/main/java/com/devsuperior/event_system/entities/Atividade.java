6a5d011e5c95d7992b81a9f4e0acee4521077477
José Marcos <josemarcosdemelolima1@gmail.com>
2026-02-13 12:58:56 -0300

    Create Bloco entity with many-to-one association to Atividade

--- src/main/java/com/devsuperior/event_system/entities/Atividade.java
+++ src/main/java/com/devsuperior/event_system/entities/Atividade.java
@@ -1,5 +1,7 @@
 package com.devsuperior.event_system.entities;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Column;
@@ -9,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -30,8 +33,10 @@ public class Atividade {
 	@JoinColumn(name="categoria_id")
 	private Categoria categoria;
 
+	@OneToMany(mappedBy = "atividade")
+	private List<Bloco> blocos = new ArrayList<>();
+	
 	public Atividade() {
-		
 	}
 	
 	public Atividade(Integer id, String nome, String descricao, Double preco, Categoria categoria) {
